bbf8884c2bb1847b4c15e2b725817aa8be009662
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 00:29:39 +0700

    Update TQTO.js

--- plugins/TQTO.js
+++ plugins/TQTO.js
@@ -3,7 +3,7 @@
 //kalo mau ada nama kalian chat dulu ke ibnu,izin dulu biar sama² enak
 //di tambahin nama boleh tapi di ilagin jangan walau satu nama pun
 //yg gk tau nomer raku nih nomer nya
-//http://wa.me/6287834993722?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
+//http://wa.me/6285828764046?text=bg%20izin%20taroh%20nama%20di%20bot%20mu%20yak
 let handler = async (m, { conn, text }) => {
   conn.reply(m.chat, `
 *Pertanyaan:* ${m.text}
